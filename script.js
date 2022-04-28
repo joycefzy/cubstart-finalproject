b1e2395224aed48a3ed34874407ad519817b50d3
Kira Cope <kira.cope@berkeley.edu>
2022-04-27 22:44:33 -0700

    Added some test code for cycling backgrounds

---
+++ script.js
@@ -0,0 +1,27 @@
+//maybe ask for your name when you first start it
+//Need an add to workload (to a workload table?)
+//Need an add to dueDate (to a dueDate table?)
+//Need an add to maps (to a maps table?)
+    //if time crunch just drop the maps I guess, or just make a link that plugs it into google maps
+
+/* Some demo code I found for changing backgrounds randomly
+<html>
+    <head>
+        <script type="text/javascript"> 
+            var totalCount = 8;
+            function ChangeIt() 
+        {
+        var num = Math.ceil( Math.random() * totalCount );
+        document.body.background = 'bgimages/'+num+'.jpg';
+        document.body.style.backgroundRepeat = "repeat";// Background repeat
+    }
+        </script>
+    </head>
+<body> 
+// Page Design 
+</body> 
+<script type="text/javascript"> 
+ChangeIt();
+</script> 
+</html>
+*/
