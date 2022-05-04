32561dc7f84a91240a3417260932eb0ea79062a0
Joycefzy <joycefzy@berkeley.edu>
2022-05-04 16:08:53 -0700

    yes

--- lists.js
+++ lists.js
@@ -1,28 +1,26 @@
 //CODE COPIED AND PASTED FROM PLAYGROUND.JS AS A STARTING POINT
 
-const todoName = document.getElementById("todo-name");
-const todoDate = document.getElementById("todo-date");
-const submitButton = document.getElementById("submit");
-const clearButton = document.getElementById("clear");
-const undoButton = document.getElementById("undo");
-const todoDict = document.getElementById("todo");
-// END PART 5
+const dueDateName = document.getElementById("task-name");
+const dueDate = document.getElementById("task-date");
+const addButton = document.getElementById("add");
+//const clearButton = document.getElementById("clear");
+//const undoButton = document.getElementById("undo");
+//const todoDict = document.getElementById("todo");
 
-// BEGIN PART 6
-
-submitButton.addEventListener("click", () => {
-    console.log("clicked submit")
-  if (todoName.value != "") {
-    const todo = document.createElement("dict");
-    todo.innerHTML = todoName.value, todoDate.value;
-    todoDict[todoName.value] = todoDate.value;
+addButton.addEventListener("click", () => {
+    console.log("clicked add")
+  if (dueDateName.value != "") {
+    const due = document.createElement("dict");
+    due.innerHTML = dueDateName.value, dueDate.value;
+    dueDict[dueDateName.value] = dueDate.value;
     //document.getElementById("todo-name").value = "";
     //put back in after testing over and do one for date too
     //Was working with text but stopped when changed to dictionary.
-    console.log(todoDict)
+    console.log(dueDict);
   }
 });
 
+/** 
 undoButton.addEventListener("click", () => {
   if (todoDict.length != 0) {
     todoDict.removeChild(todoDict.lastElementChild);
@@ -35,3 +33,4 @@ clearButton.addEventListener("click", () => {
   todoName.value = "";
   document.getElementById("todo-name").value = "";
 });
+*/
